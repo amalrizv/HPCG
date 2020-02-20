bd3af15470f785ad25df7aece5ae6d9ec8e1100c
Amal Rizvi <amalrizv@gmail.com>
2020-02-20 05:20:03 +0000

    Fixed length of arrays distinct_factors and count_factors

--- src/ComputeOptimalShapeXYZ.jl
+++ src/ComputeOptimalShapeXYZ.jl
@@ -86,8 +86,12 @@ function compute_optimal_shape_xyz(xyz, x, y, z)
   elseif length(factors) == 3 && factors[x] == 1 && factors[y] == 1 && keyss[3] == 1# three distinct and single prime factors
     z = keyss[1]
   else  # 3 or more prime factors so try all possible 3-subsets
-    distinct_factors = keyss 
-    count_factors = vals
+    distinct_factors = Array{Int64,1}(undef, 33)
+    count_factors = Array{Int64,1}(undef, 33)
+    for i =1: length (keyss)
+	distinct_factors = keyss[i] 
+    	count_factors = vals[i]
+    end
 
     # count total number of prime factors in "c_main" and distribute some factors into "c1"
     c_main = MBCounter(count_factors, length(factors))
