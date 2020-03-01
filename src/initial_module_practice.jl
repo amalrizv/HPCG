79ceb7f36e111c1094afa5728941a0d95e678f9e
Amal Rizvi <amalrizv@gmail.com>
2020-03-01 08:56:20 +0000

    initial module file

---
+++ src/initial_module_practice.jl
@@ -0,0 +1,15 @@
+__precompile__()
+
+# precompile module 
+
+module HPCG_Julia
+# no indentation in this file 
+# include all files here ?
+# use all packages here
+# create other modules for CGData TestNormsData, TestSymmetryData, MGData, 
+# HPCGSparseMatrix, TestCGData, Geometry or include them all here
+# along with their constructors
+# add appendx.jl which includes the only
+# function in Vector.hpp that we ported
+
+end # module
