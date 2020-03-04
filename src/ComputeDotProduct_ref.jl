6d1d490e98bffee69ccedb5a3e24098471640dcb
Amal Rizvi <amalrizv@gmail.com>
2020-03-04 22:04:39 +0000

    Fixing ComputeDotProduct_Ref all reduce

--- src/ComputeDotProduct_ref.jl
+++ src/ComputeDotProduct_ref.jl
@@ -26,24 +26,24 @@ function compute_dot_product_ref!(n::Int64, x::Array{Float64,1}, y::Array{Float6
   @assert(length(x)>=n) # Test vector lengths
   @assert(length(y)>=n)
 
-  local_result = 0.0
-  result       = 0.0
+  local_result = [0.0]
+  result       = [0.0]
   time_allreduce= 0.0
   for i=1:n
   	if x==y
-        local_result += x[i]*x[i]
+        local_result[1] += x[i]*x[i]
   	else 
-        local_result += x[i]*y[i]
+        local_result[1] += x[i]*y[i]
   	end
  end
 
   if MPI.Initialized()
  	#Use MPI's reduce function to collect all partial sums
   	t0 = time_ns()
-  	global_result = 0.0
+  	global_result = [0.]0
   
   	MPI.Allreduce!(local_result, global_result, 1, MPI.SUM, MPI.COMM_WORLD)
-  	result = global_result
+  	result = global_result[1]
   	time_allreduce += time_ns() - t0
   else 
   	time_allreduce += 0.0
